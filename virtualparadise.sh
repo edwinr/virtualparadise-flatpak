2da56922f370a40162c50cab365769aa8dd4a903
Edwin Rijkee <edwin@virtualparadise.org>
2023-12-29 17:46:57 +0100

    Fix reading/writing of virtualparadise.cfg

--- virtualparadise.sh
+++ virtualparadise.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export ZYPAK_CEF_LIBRARY_PATH=/app/lib/libcef.so
+cd $XDG_CONFIG_HOME # because the config file is written to the working directory
 exec zypak-wrapper virtualparadise.bin
